9eb1d8e73c1719f0912afab41f2b92e84c114902
Liz Cooper <eacooper400@gmail.com>
2017-11-27 14:33:03 -0500

    update R cluster script

--- Scripts/11_forCluster.R
+++ Scripts/11_forCluster.R
@@ -1,20 +1,26 @@
 ### Set the Library Path to Install and
 ### Load any Packages; install devtools
 .libPaths(c('~/compGenWS_112017/Rlibs', .libPaths()))
+install.packages("devtools", repos='http://cran.us.r-project.org')
 
-### Set working directory
-setwd("~/Desktop/Teaching/CompGenWS-GEN8900/SampleData")
-options(stringsAsFactors=FALSE)
+### Use command line arguments to specify input and output files,
+### as well as the snp positions to look at
+args = commandArgs(trailingOnly=TRUE)
+input = args[1]
+output = args[2]
+snp1 = as.numeric(args[3])
+snp2 = as.numeric(args[4])
 
-### Read in the my functions file
+### Read in my functions file,
+### set the strings as factors = FALSE
 library(devtools)
 source_gist("c954c757db02eb6c4ccfae1aa090658c", filename="compGen17_fxns.R", quiet=TRUE)
+options(stringsAsFactors=FALSE)
 
-### Read in the sample data
-my.data=read.vcf("heli_phased.vcf", header=TRUE)
-
-### Set the "anchor" SNP as 700,962
-snp.of.interest = my.data[which(my.data$POS==700962),]
+### Read in the sample data, then find the positions associated with the 2 SNPs
+my.data=read.vcf(input, header=TRUE)
+snp.of.interest = my.data[which(my.data$POS==snp1),]
+control.snp = my.data[which(my.data$POS==snp2),]
 
 ### Calculate R-squared between the SNP of
 ### interest and ALL other SNPs in the data
@@ -30,16 +36,7 @@ for (i in 1:nrow(smoothed.results)) {
         & my.results$Pos<smoothed.results$BinEnds[i]))
     smoothed.results$AvgRsq[i] = mean(w$Rsq, na.rm=TRUE)
 }
-
-### Plot the results,
-### add a star where the SNP of interest is
-plot(smoothed.results$BinStarts, smoothed.results$AvgRsq,
-     xlab="Position (bp)", ylab="Linkage Disequilibrium (r2)",
-     type="l", col="red", ylim=c(0,1))
-points(snp.of.interest[,2], 1, pch=8, col="red", cex=2)
-
 ### Now, pick a control SNP and do the same thing
-control.snp = my.data[which(my.data$POS==1531867),]
 my.results$Rsq2 = apply(my.data, 1, function(x,y)
     calc_r2(as.vector(x, mode="character"), as.vector(y, mode="character")), y=control.snp)
 smoothed.results$AvgRsq2=0
@@ -48,5 +45,22 @@ for (i in 1:nrow(smoothed.results)) {
         & my.results$Pos<smoothed.results$BinEnds[i]))
     smoothed.results$AvgRsq2[i] = mean(w$Rsq2, na.rm=TRUE)
 }
+
+### Save the results to an output file
+write.table(smoothed.results, paste0(output, "txt", collapse="."), quote=FALSE,
+            row.names=FALSE, col.names=TRUE, sep="\t")
+
+### Open a PDF file to plot the results in
+pdf(paste0(output, "pdf", collapse="."))
+
+### Plot the results,
+### add a star where the SNP of interest is
+plot(smoothed.results$BinStarts, smoothed.results$AvgRsq,
+     xlab="Position (bp)", ylab="Linkage Disequilibrium (r2)",
+     type="l", col="red", ylim=c(0,1))
+points(snp.of.interest[,2], 1, pch=8, col="red", cex=2)
 lines(smoothed.results$BinStarts, smoothed.results$AvgRsq2, col="blue")
 points(control.snp[,2], 1, pch=8, col="blue", cex=1.5)
+
+### Close the connection to the pdf file!
+dev.off()
